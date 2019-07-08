d87f026493aa10e95ad27f3ca6377849d8aa0242
Jesse Reynolds <jesse@va.com.au>
2019-07-08 21:48:53 +0930

    simplify fixup_value

--- lib/puppet_x/secedit.rb
+++ lib/puppet_x/secedit.rb
@@ -1,16 +1,13 @@
 require_relative 'lsp/security_policy.rb'
+#require 'fileutils'
 
 module PuppetX
   module Secedit
     # converts any values that might be of a certain type specified in the mapping
     # converts everything to a string
     # returns the value
-    def self.fixup_value(value, type)
-      value = value.to_s.strip
-      case type
-        when :quoted_string
-          value = "\"#{value}\""
-      end
+    def self.fixup_value(value)
+      #value = value.to_s.strip
       value
     end
 
@@ -24,7 +21,7 @@ module PuppetX
       # puts "Exporting policies to inifile #{inffile_winpath}"
 
       Facter::Core::Execution.execute("secedit /export /cfg #{inffile_winpath} /quiet")
-
+      #FileUtils.cp(inffile_winpath, '.')
       unless File.exist?(inffile_winpath)
         raise "secedit export file does not exist at #{inffile_winpath}"
       end
@@ -33,8 +30,11 @@ module PuppetX
       File.open tempfile, 'r:IBM437' do |file|
         content_ibm437 = file.read
         content_utf8 = content_ibm437.encode('utf-8')
-        @inifile = PuppetX::Inifile.new(:content => content_utf8)
+        # File.open('ibm437-to-utf8.txt', 'w') { |file| file.write(content_utf8) }
+        @inifile = PuppetX::Inifile.new(:content => content_utf8, :encoding => 'utf-8')
       end
+
+      #@inifile.write(:filename => 'secedit_debug.ini')
 
       policies = {}
 
@@ -50,10 +50,10 @@ module PuppetX
           ].include?(parameter_name)
           policy_desc, policy_values = SecurityPolicy.find_mapping_from_policy_name(parameter_name)
           policy_hash = {
-              :ensure => ensure_value,
-              :policy_type => section ,
+              :ensure         => ensure_value,
+              :policy_type    => section ,
               :policy_setting => parameter_name,
-              :policy_value => fixup_value(parameter_value, policy_values[:data_type])
+              :policy_value   => fixup_value(parameter_value)
           }
           policies[policy_desc] = policy_hash
         end
